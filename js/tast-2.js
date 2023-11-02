3953933a99a0f4d10f16b8aa795bb8c19a5ba3e9
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2023-11-02 11:41:53 +0200

    Update tast-2.js

--- js/tast-2.js
+++ js/tast-2.js
@@ -7,9 +7,13 @@
 // console.log(getLength(["M", "a", "n", "g", "o"]));
 // console.log(getLength(["top", "picks", "for", "you"]));
 
-function calculateEngravingPrice(message, pricePerWord) {
-	const messageSplit = message.split(" ");
-	const messageLenght = messageSplit.length;
-	return messageLenght * pricePerWord;
-}
-console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+// function calculateEngravingPrice(message, pricePerWord) {
+// 	const messageSplit = message.split(" ");
+// 	const messageLenght = messageSplit.length;
+// 	return messageLenght * pricePerWord;
+// }
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+let firstName = null;
+let nickName = "Суперкодер";
+
+console.log(firstName ?? nickName ?? "Анонім");
