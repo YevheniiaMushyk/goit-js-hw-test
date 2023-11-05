11971a8c92acca2c7c1dbc8a5165d1689345137e
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2023-11-05 18:14:08 +0200

    Update test-2.js

--- js/test-2.js
+++ js/test-2.js
@@ -1,13 +1,28 @@
-/*
- * Напиши скрипт, який порівнює числа змінних a і b. Якщо обидва значення більше 100, виведи в консоль максимальне з них. Інакше у консолі має бути сума значення b та числа 512.
- */
+// function getEvenNumbers(start, end) {
+// 	const array = [];
+// 	for (let index = start; index <= end; index++) {
+// 		if (index % 2 === 0) {
+// 			array.push(index);
+// 		} else {
+// 		}
+// 	}
+// 	return array;
+// }
+// console.log(getEvenNumbers(6, 12));
 
-const a = 120;
-const b = 250;
-
-let result = 0;
-if (a > 100 && b > 100) {
-	console.log(Math.max(a, b));
-} else {
-	console.log(b + 512);
+function getCommonElements(array1, array2) {
+	const array3 = [];
+	let item;
+	for (let index = 0; index < array1.length; index++) {
+		item = array1[index];
+		for (let index2 = 0; index2 < array2.length; index2++) {
+			if (item === array2[index2]) {
+				array3.push(item);
+			} else {
+			}
+		}
+	}
+	return array3;
 }
+
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])); //повертає [12, 27, 3));
