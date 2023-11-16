118b2b7cc139f78ef38d18c2270189787739d24a
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2023-11-16 11:42:14 +0200

    fgdfdeeed

--- js/test-3.js
+++ js/test-3.js
@@ -20,13 +20,37 @@
 // 	return checkValue >= 0 ? array.slice(0, index) : [];
 // }
 
-function createArrayOfNumbers(min, max) {
-	const array = [];
+//
 
-	for (let i = min; i <= max; i += 1) {
-		array.push(i);
+let count = 0;
+
+function cc(card) {
+	// Змініть код лише під цим рядком
+	switch (card) {
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			count += 1;
+			break;
+		case 7:
+		case 8:
+		case 9:
+			break;
+		case 10:
+		case "J":
+		case "Q":
+		case "K":
+		case "A":
+			count -= 1;
+			break;
 	}
-	return array;
+	return count > 0 ? `${count} Bet` : `${count} Hold`;
+
+	// Змініть код лише над цим рядком
 }
 
-console.log(createArrayOfNumbers(14, 17));
+cc(7);
+cc(8);
+cc(9);
